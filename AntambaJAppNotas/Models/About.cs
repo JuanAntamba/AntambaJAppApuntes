848140aa64d7cd31d5fdaccb777a0190196c95ac
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2025-05-31 00:40:55 -0500

    Cambios de diseño, Requerimientos finales de la tarea, descripción personasl, encabezado personalizado.

--- AntambaJAppNotas/Models/About.cs
+++ AntambaJAppNotas/Models/About.cs
@@ -8,9 +8,11 @@ namespace AntambaJAppNotas.Models
 {
     internal class About
     {
-        public string Title => AppInfo.Name;
+    
+        public string Title => "Juan Carlos Antamba";
         public string Version => AppInfo.VersionString;
         public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string Message =>
+            "Soy Ingeniero en Software, me gustaría especializarme en desarrollo Front-End. Me gustan los deportes, especialmente el fútbol, y mi equipo favorito es Liga de Quito.";
     }
 }
